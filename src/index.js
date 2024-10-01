a7eaa31ea9cb058b0896104072e648aa86ce3400
alexanderpo <alexander.pozhidaev@gmail.com>
2024-10-01 17:16:14 +0700

    refactor

--- src/index.js
+++ src/index.js
@@ -5,55 +5,81 @@ const config = require("config");
 const express = require("express");
 const vhost = require("vhost");
 
-function createVirtualHost(app, { host, folder, ssl }) {
-  if (ssl) {
-    app.use(function (request, response, next) {
-      if (!request.secure) {
-        return response.redirect(
-          "https://" + request.headers.host + request.url
-        );
-      }
-      next();
-    });
-  }
-  const root = path.resolve(__dirname, path.resolve("sites", folder));
-  app.use(vhost(host, express.static(root)));
+const cwd = process.cwd();
+
+function redirectToUrl(app, { host, redirectTo }) {
   app.use(
     vhost(host, (req, res) => {
-      res.sendFile("index.html", { root });
+      return res.redirect(redirectTo);
+    })
+  );
+}
+
+function redirectToHttps(app, { host }) {
+  app.use(
+    vhost(host, (req, res) => {
+      return res.redirect("https://" + req.headers.host + req.url);
+    })
+  );
+}
+
+function virtualHost(app, { host, folder, spa }) {
+  const root = path.resolve(cwd, "sites", folder);
+  app.use(vhost(host, express.static(root, { extensions: ["html"] })));
+  app.use(
+    vhost(host, (req, res) => {
+      res.sendFile(spa ? "index.html" : "404.html", { root });
     })
   );
 }
 
 console.log("sites:");
-config.get("sites").forEach((site) => {
-  console.log(site.host);
+
+const sites = config.get("sites");
+
+sites.forEach((site) => {
+  console.log(site.ssl ? `${site.host} (ssl)` : site.host);
 });
 
 console.log("------------------------");
 
-//Start server
 const port = config.get("port");
 
 if (port.http) {
   const app = express();
-  config.get("sites").forEach((site) => createVirtualHost(app, site));
+  sites.forEach((site) => {
+    if (site.redirectTo) {
+      redirectToUrl(app, site);
+    } else if (site.ssl) {
+      redirectToHttps(app, site);
+    } else {
+      virtualHost(app, site);
+    }
+  });
   app.listen(port.http, () => {
-    console.log("web server listening on port %d", port.http);
+    console.log("http server listening on port %d", port.http);
   });
 }
 
 if (port.https) {
   const app = express();
-  config.get("sites").forEach((site) => createVirtualHost(app, site));
+  sites
+    .filter((x) => x.ssl)
+    .forEach((site) => {
+      if (site.redirectTo) {
+        redirectToUrl(app, site);
+      } else {
+        virtualHost(app, site);
+      }
+    });
   const sslServer = https.createServer(
     {
-      key: fs.readFileSync(path.join(process.cwd(), "cert", "key.pem")),
-      cert: fs.readFileSync(path.join(process.cwd(), "cert", "cert.pem")),
+      key: fs.readFileSync(path.resolve(cwd, "cert", "key.pem")),
+      cert: fs.readFileSync(path.resolve(cwd, "cert", "cert.pem")),
     },
     app
   );
   sslServer.listen(port.https, () => {
-    console.log("ssl server listening on port %d", port.https);
+    console.log("https server listening on port %d", port.https);
   });
 }
