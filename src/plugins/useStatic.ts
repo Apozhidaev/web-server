c38d62dd29f80b94be941429d274c75d303925f6
alexanderpo <alexander.pozhidaev@gmail.com>
2024-12-10 02:21:01 +0700

    add: max-age=60 for .png

--- src/plugins/useStatic.ts
+++ src/plugins/useStatic.ts
@@ -14,4 +14,13 @@ export function UseStaticPlugin(
       decorateReply: i === 0,
     });
   }
+  if (staticSites.length > 0) {
+    fastify.addHook("onSend", (req, reply, payload, done) => {
+      const err = null;
+      if (req.url.endsWith(".png")) {
+        reply.header("cache-control", "public, max-age=60");
+      }
+      done(err, payload);
+    });
+  }
 }
